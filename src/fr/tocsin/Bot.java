c37163799bb78d3bf52d7a73b574e3524fa1cd02
Serge <serge@serge.wf>
2017-09-30 12:01:15 -0500

    Sanitized code for PII, moved properties to file

--- src/fr/tocsin/Bot.java
+++ src/fr/tocsin/Bot.java
@@ -41,15 +41,15 @@ public class Bot implements MessengerCallback {
         /*
         Portfolio pf = new Portfolio("Test", new User());
 
-        Position po1 = new Position("MORL", 157, 14.88);
-        Position po2 = new Position("SDOG", 102, 35.23);
-        Position po3 = new Position("BNDX", 36, 53.54);
-        Position po4 = new Position("DGS", 97, 40.06);
-        Position po5 = new Position("RWX", 59, 40.20);
-        Position po6 = new Position("DFE", 56, 52.66);
-        Position po7 = new Position("FYT", 78, 29.51);
-        Position po8 = new Position("FV", 75, 22.91);
-        Position po9 = new Position("AMU", 115, 19.34);
+        Position po1 = new Position("MORL", 1, 14.88);
+        Position po2 = new Position("SDOG", 2, 35.23);
+        Position po3 = new Position("BNDX", 3, 53.54);
+        Position po4 = new Position("DGS", 4, 40.06);
+        Position po5 = new Position("RWX", 5, 40.20);
+        Position po6 = new Position("DFE", 6, 52.66);
+        Position po7 = new Position("FYT", 7, 29.51);
+        Position po8 = new Position("FV", 8, 22.91);
+        Position po9 = new Position("AMU", 9, 19.34);
 
         pf.addPosition(po1);
         pf.addPosition(po2);
--- src/fr/tocsin/channel/Messenger.java
+++ src/fr/tocsin/channel/Messenger.java
@@ -11,12 +11,13 @@ import static spark.Spark.post;
 public class Messenger {
 
     private MessengerCallback callback;
-    private String channelID = "messenger:491275257908762";
+    private String myChannelID;
 
     public Messenger(MessengerCallback cb) {
         this.callback = cb;
 
-        port(9000);
+        Properties cfg = new Properties();
+        port(Integer.parseInt(cfg.getProperty("messenger.port")));
 
         post("/input", (request, response) -> {
             callback.input(request.body());
@@ -37,11 +38,13 @@ public class Messenger {
         String req = "https://api.twilio.com/2010-04-01/Accounts/" + cfg.getProperty("twilio.sid") + "/Messages.json";
 
         try {
+            String userChannelID = myChannelID = cfg.getProperty("tmp.user.id"); // FIXME needs DB integration
+            myChannelID = cfg.getProperty("messenger.id");
             res = Unirest.post(req)
                     .header("content-type", "application/x-www-form-urlencoded")
                     .header("authorization", "Basic " + cfg.getProperty("twilio.api"))
                     .header("cache-control", "no-cache")
-                    .body("To=messenger%3A1786894171351067&From=" + channelID + "&Body=" + body)
+                    .body("To=" + userChannelID + "&From=" + myChannelID + "&Body=" + body)
                     .asString();
             data = res.getBody();
         } catch (UnirestException e) {
